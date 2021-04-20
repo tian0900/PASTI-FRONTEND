995161d75a138e6be6cd89dd11592d931bf76be7
Samuel Prayoga Tampubolon <80253461+Nero146@users.noreply.github.com>
2021-04-20 14:29:50 +0700

    Update contact.blade.php

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -40,25 +40,25 @@
     <header class="">
       <nav class="navbar navbar-expand-lg">
         <div class="container">
-          <a class="navbar-brand" href="index.blade.php"><h2>Sixteen <em>Clothing</em></h2></a>
+          <a class="navbar-brand" href="#"><h2>Sixteen <em>Clothing</em></h2></a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav ml-auto">
               <li class="nav-item">
-                <a class="nav-link" href="index.blade.php">Home
+                <a class="nav-link" href="#">Home
                   <span class="sr-only">(current)</span>
                 </a>
               </li> 
               <li class="nav-item">
-                <a class="nav-link" href="products.blade.php">Our Products</a>
+                <a class="nav-link" href="#">Our Products</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="about.hblade.php">About Us</a>
+                <a class="nav-link" href="#">About Us</a>
               </li>
               <li class="nav-item active">
-                <a class="nav-link" href="contact.blade.php">Contact Us</a>
+                <a class="nav-link" href="#">Contact Us</a>
               </li>
             </ul>
           </div>
