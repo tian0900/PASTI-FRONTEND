0dbe54b731eaecfb619d9ac723732bd21f45dfde
Onra Imanuel Sihombing <82330355+onraimanuel@users.noreply.github.com>
2021-04-20 14:20:10 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+Route::get('/menu', function () {
+    return view('menu');
+});
+Route::get('/contact', function () {
+    return view('contact');
+});
+Route::get('/login', function () {
+    return view('login');
+});
