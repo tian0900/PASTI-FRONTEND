763deecec5fc914482cb452c28d5e84c1d2e397c
Samuel Prayoga Tampubolon <80253461+Nero146@users.noreply.github.com>
2021-04-22 10:48:19 +0700

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -58,6 +58,9 @@
               <li class="nav-item">
                 <a class="nav-link" href="/contact">Contact Us</a>
               </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/contact">Login</a>
+              </li>
             </ul>
           </div>
         </div>
