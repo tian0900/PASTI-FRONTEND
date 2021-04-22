7bc7b30e4c42d9e790b390f05557915bc9faa7a7
Samuel Prayoga Tampubolon <80253461+Nero146@users.noreply.github.com>
2021-04-22 11:55:10 +0700

    Add files via upload

---
+++ resources/views/formLoginAdmin.blade.php
@@ -0,0 +1,48 @@
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<head><link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css"/>
+</head>
+<style>
+      .container{
+        border:2px solid rgb(0, 0, 0);
+        text-align:center;
+        height:400px;
+        width:400px;
+        background-color: whitesmoke;
+    }
+    body{
+        padding:60px;
+        background-image: url(https://getwallpapers.com/wallpaper/full/c/6/9/1555681.jpg);
+    }
+    h1{
+        margin:auto;
+        color: rgb(219, 63, 63);
+    }
+    .row{
+        height:90px;
+        width:396px;
+        background-color:rgb(0, 0, 0);
+    }
+    </style>
+<div class="container">
+	<div class="row">
+		 <h1><i class="fa fa-users-cog" aria-hidden="true"></i> Login Admin</h1>
+        </div><br /><br />
+                	<div class="input-group">
+								<div class="input-group-prepend">
+									<span class="input-group-text"><i class="fas fa-user-tie"></i></span>
+								</div>
+								<input type="text" name="" class="form-control" placeholder="username"/>
+							</div><br />
+
+                	<div class="input-group">
+								<div class="input-group-prepend">
+									<span class="input-group-text"><i class="fa fa-key icon"></i></span>
+								</div>
+									<input type="Password" name="" class="form-control" placeholder="password"/>
+							</div><br />
+              <button type="submit" class="btn btn-success"><span class="glyphicon glyphicon-off"></span> Login</button>
+	</div>
