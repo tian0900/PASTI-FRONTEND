c9c11e146ebfbe3dfa82e4272d0415068affdbba
Samuel Prayoga Tampubolon <80253461+Nero146@users.noreply.github.com>
2021-05-04 13:26:01 +0700

    iejdm

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -87,34 +87,6 @@
               </form>
             </div>
           </div>
-          <div class="col-md-4">
-            <ul class="accordion">
-              <li>
-                  <a>Accordion Title One</a>
-                  <div class="content">
-                      <p>Lorem ipsum dolor sit amet, consectetur adipisic elit. Sed voluptate nihil eumester consectetur similiqu consectetur.<br><br>Lorem ipsum dolor sit amet, consectetur adipisic elit. Et, consequuntur, modi mollitia corporis ipsa voluptate corrupti elite.</p>
-                  </div>
-              </li>
-              <li>
-                  <a>Second Title Here</a>
-                  <div class="content">
-                      <p>Lorem ipsum dolor sit amet, consectetur adipisic elit. Sed voluptate nihil eumester consectetur similiqu consectetur.<br><br>Lorem ipsum dolor sit amet, consectetur adipisic elit. Et, consequuntur, modi mollitia corporis ipsa voluptate corrupti elite.</p>
-                  </div>
-              </li>
-              <li>
-                  <a>Accordion Title Three</a>
-                  <div class="content">
-                      <p>Lorem ipsum dolor sit amet, consectetur adipisic elit. Sed voluptate nihil eumester consectetur similiqu consectetur.<br><br>Lorem ipsum dolor sit amet, consectetur adipisic elit. Et, consequuntur, modi mollitia corporis ipsa voluptate corrupti elite.</p>
-                  </div>
-              </li>
-              <li>
-                  <a>Fourth Accordion Title</a>
-                  <div class="content">
-                      <p>Lorem ipsum dolor sit amet, consectetur adipisic elit. Sed voluptate nihil eumester consectetur similiqu consectetur.<br><br>Lorem ipsum dolor sit amet, consectetur adipisic elit. Et, consequuntur, modi mollitia corporis ipsa voluptate corrupti elite.</p>
-                  </div>
-              </li>
-            </ul>
-          </div>
         </div>
       </div>
     </div>
